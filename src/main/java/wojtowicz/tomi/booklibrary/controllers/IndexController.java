9555dc2f401722b34d2cf22b42d3f95d55b1dfbd
Tommy <tomi.git@gmail.com>
2017-07-06 18:16:59 +0200

    Initial commit

---
+++ src/main/java/wojtowicz/tomi/booklibrary/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package wojtowicz.tomi.booklibrary.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by tommy on 7/6/2017.
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping
+    String index() {
+        return "index";
+    }
+}
