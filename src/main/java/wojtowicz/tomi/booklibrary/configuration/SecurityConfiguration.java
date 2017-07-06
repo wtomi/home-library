a12da6b01dd62c51b5880b3e1f92b9f534271a71
Tommy <tomi.git@gmail.com>
2017-07-06 18:17:42 +0200

    Add simple configuration of SpringBoot Security

---
+++ src/main/java/wojtowicz/tomi/booklibrary/configuration/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package wojtowicz.tomi.booklibrary.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by tommy on 7/6/2017.
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+}
