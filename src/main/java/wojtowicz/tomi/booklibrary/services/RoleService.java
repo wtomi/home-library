d23d1702db899f71457e16ac50834ba832a8f271
Tommy <tomi.git@gmail.com>
2017-07-09 11:27:57 +0200

    Add RoleSerivce interface

---
+++ src/main/java/wojtowicz/tomi/booklibrary/services/RoleService.java
@@ -0,0 +1,9 @@
+package wojtowicz.tomi.booklibrary.services;
+
+import wojtowicz.tomi.booklibrary.domain.Role;
+
+/**
+ * Created by tommy on 7/9/2017.
+ */
+public interface RoleService extends CRUDService<Role>{
+}
