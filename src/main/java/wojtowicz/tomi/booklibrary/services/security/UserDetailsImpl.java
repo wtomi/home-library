8179fa80875aec18a6fc05a2fcb7cabc9140f6d8
Tommy <tomi.git@gmail.com>
2017-07-09 12:41:54 +0200

    Add UserDetailsImpl

---
+++ src/main/java/wojtowicz/tomi/booklibrary/services/security/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package wojtowicz.tomi.booklibrary.services.security;
+
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Created by tommy on 7/9/2017.
+ */
+@Setter
+public class UserDetailsImpl implements UserDetails {
+
+    private Collection<SimpleGrantedAuthority> authorities;
+
+    private String username;
+
+    private String password;
+
+    private Boolean enabled = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
