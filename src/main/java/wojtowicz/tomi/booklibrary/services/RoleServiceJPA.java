414e0897ce96f81f384c11f8dee19e9f29b33265
Tommy <tomi.git@gmail.com>
2017-07-09 12:04:23 +0200

    Add RoleSeriveJPA implementation

---
+++ src/main/java/wojtowicz/tomi/booklibrary/services/RoleServiceJPA.java
@@ -0,0 +1,39 @@
+package wojtowicz.tomi.booklibrary.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import wojtowicz.tomi.booklibrary.domain.Role;
+import wojtowicz.tomi.booklibrary.repositories.RoleRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tommy on 7/9/2017.
+ */
+public class RoleServiceJPA implements RoleService{
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public List<?> listAll() {
+        List<Role> roles = new ArrayList<>();
+        roleRepository.findAll().forEach(roles::add);
+        return roles;
+    }
+
+    @Override
+    public Role getById(Integer id) {
+        return roleRepository.findOne(id);
+    }
+
+    @Override
+    public Role SaveOrUpdate(Role domainObject) {
+        return roleRepository.save(domainObject);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        roleRepository.delete(id);
+    }
+}
