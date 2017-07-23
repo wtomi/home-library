6fb9eeb464f3f545133c466a71c2f70629281667
Tommy <tomi.git@gmail.com>
2017-07-23 22:40:36 +0200

    Add getters and setters

--- src/main/java/wojtowicz/tomi/booklibrary/domain/Book.java
+++ src/main/java/wojtowicz/tomi/booklibrary/domain/Book.java
@@ -1,10 +1,14 @@
 package wojtowicz.tomi.booklibrary.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 
+@Getter
+@Setter
 @Entity
 public class Book {
 
--- src/main/java/wojtowicz/tomi/booklibrary/domain/Library.java
+++ src/main/java/wojtowicz/tomi/booklibrary/domain/Library.java
@@ -1,7 +1,12 @@
 package wojtowicz.tomi.booklibrary.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 
+@Getter
+@Setter
 @Entity
 public class Library extends AbstractDomainClass {
 
